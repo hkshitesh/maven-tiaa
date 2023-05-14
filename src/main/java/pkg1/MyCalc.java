d5939bf364b061bd352bd0d44b8c73b99bfdb78d
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2023-05-14 12:46:44 +0530

    Junit test case added

--- src/main/java/pkg1/MyCalc.java
+++ src/main/java/pkg1/MyCalc.java
@@ -1,50 +1,20 @@
 //Added comment
 package pkg1;
 public class MyCalc {	
-	int sum(int a, int b)
+	public int sum(int a, int b)
 	{
 		return(a+b);
 	}
-	int diff(int a, int b)
+	public int diff(int a, int b)
 	{
 		return(a-b);
 	}
-	int mul(int a, int b)
+	public int mul(int a, int b)
 	{
 		return(a*b);
 	}
-	int div(int a, int b)
+	public int div(int a, int b)
 	{
 		return(a/b);
 	}
-	void sayHello()
-	{
-		System.out.println("Hello TIAA");
-	}
-
 }
-
-	
-  
-  
-  
-  void SayBye()
-	{
-		System.out.println("Bye");
-	}
-}
-
-
-
-
-
-
-
-
-	void sayBye2()
-	{
-		System.out.println("Bye UPES");
-	}
-
-}
-
